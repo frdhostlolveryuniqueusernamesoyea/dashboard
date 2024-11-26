739949a0f2db261bd9e63a376e6c1693168a6594
MrTomiCZ <131264466+MrTomiCZ@users.noreply.github.com>
2024-11-26 13:57:24 +0100

    gigantic update

---
+++ js/auth.js
@@ -0,0 +1,30 @@
+// set the variables
+const params = new URLSearchParams(window.location.search);
+const email = params.get('email');
+const usr = params.get('username');
+const passw = params.get('password');
+
+
+// send rekvests
+async function sendData() {
+    const request = new Request("jpman url tady", {
+        method: "POST",
+        body: JSON.stringify({ 
+            username: username,
+            password: passw,
+            email: email
+         }),
+      });
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`faklo se to: ${response.status}`);
+      }
+  
+      const json = await response.json();
+      console.log("lesgo:", json);
+    } catch (error) {
+      console.error("njn faklo se to:", error.message);
+    }
+  }
+  
